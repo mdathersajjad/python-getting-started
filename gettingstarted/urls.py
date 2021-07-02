c6c2f2660243aadc46b74d333b9ba4f0e6cc8c4e
Ather Sajjad <athersajjad@glenwoodsystems.com>
2021-07-02 13:47:53 +0530

    fix

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -31,7 +31,6 @@ class LetterdropProxy(HttpProxy):
         'djproxy.proxy_middleware.AddXFF',
         'djproxy.proxy_middleware.AddXFH',
         'djproxy.proxy_middleware.AddXFP',
-        'djproxy.proxy_middleware.ProxyPassReverse',
         'hello.letterdrop_proxy.AddXFS'
     ]
 
@@ -40,8 +39,7 @@ class LetterdropAssetProxy(HttpProxy):
     proxy_middleware = [
         'djproxy.proxy_middleware.AddXFF',
         'djproxy.proxy_middleware.AddXFH',
-        'djproxy.proxy_middleware.AddXFP',
-        'djproxy.proxy_middleware.ProxyPassReverse'
+        'djproxy.proxy_middleware.AddXFP'
     ]
 
 urlpatterns = [
