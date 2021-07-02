0a5077bf7687d5df88f2527d564ed58a344d4f48
Ather Sajjad <athersajjad@glenwoodsystems.com>
2021-07-02 15:41:05 +0530

    test

--- hello/letterdrop_proxy.py
+++ hello/letterdrop_proxy.py
@@ -4,13 +4,4 @@ class AddXFS(object):
 
     def process_request(self, proxy, request, **kwargs):
         kwargs['headers']['X-Forwarded-Server'] = "tranquil-atoll-99599.herokuapp.com";
-
         return kwargs
-
-    def process_response(self, proxy, request, upstream_response, response):
-        #print(response.content);
-        print("Upstream response");
-        print(upstream_response.content);
-        #print(response.status_code);
-        #print(upstream_response.status_code)
-        return response;
